b56d4a75b0cf43030f113f354cad7175b9d7ae66
JulienToons <32437624+JulienToons@users.noreply.github.com>
2019-05-18 10:38:08 -0700

    Update game.js

--- carrots/game.js
+++ carrots/game.js
@@ -607,7 +607,7 @@ Game.World.prototype = {
 		//console.log(`Apparent Carrots Before: ${this.carrots.length}`); 
 		//console.log(`carr x would be ${this.carrots[this.carrots.indexOf(carrot)].base_x}`);
 		
-	  //  let carr = this.carrots[this.carrots.indexOf(carrot)];
+	    let carr = this.carrots[this.carrots.indexOf(carrot)];
 	//	this.eatenCarrots.splice(0,0,new Game.deadCarrot([Math.floor((carr.base_x - 5)/this.tile_set.tile_size) , Math.floor((carr.base_y + 5)/this.tile_set.tile_size)], this.zone_id));
 
 		this.carrots.splice(this.carrots.indexOf(carrot), 1);
